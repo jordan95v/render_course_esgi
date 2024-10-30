c63c490eb698c2ef371e9397227ffc3603b20c9a
jordan95v <90615280+jordan95v@users.noreply.github.com>
2024-10-30 10:50:47 +0100

    Added db.

--- db.js
+++ db.js
@@ -1,8 +1,14 @@
 const { Sequelize } = require('sequelize')
 
+const DB_USER = process.env.DB_USER
+const DB_PASS = process.env.DB_PASS
+const DB_HOST = process.env.DB_HOST
+const DB_PORT = process.env.DB_PORT
+const DB_NAME = process.env.DB_NAME
+
 // Database
 const sequelize = new Sequelize(
-  '', // TODO: database connection string
+  `postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
   {
     dialect: 'postgres',
     dialectOptions: {
